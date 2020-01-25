6b26757a4797af8aa00cd5cb1fb402489fcaef09
pogikeith <pogikeith@yahoo.com>
2020-01-25 16:40:23 -0700

    strings

---
+++ files/strings.py
@@ -0,0 +1,28 @@
+# str_h = "hello world"
+
+# print(str_h)
+
+# len('hello') 
+
+mystring = 'hello world'
+print(mystring[:3])
+
+
+mystring = 'hello world'
+print(mystring[0:4])
+
+mystring = 'abcdef'
+print(mystring[1:4])
+
+
+mystring = 'abcdefg'
+print(mystring[1:])
+
+mystring = 'abcdefg'
+print(mystring[::])
+
+mystring = 'abcdefg'
+print(mystring[::3])
+
+mystring = 'abcdefg'
+print(mystring[::-1])
