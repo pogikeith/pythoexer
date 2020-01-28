f71f5a0cfff692240963ed7a8b7aab5df8d79ae2
pogikeith <pogikeith@yahoo.com>
2020-01-28 16:32:34 -0700

    split

--- files/stringslicing.py
+++ files/stringslicing.py
@@ -9,3 +9,19 @@ print(name + 'p')
 x= 'helloworld'
 
 print(x + 'it is awesome')
+
+letter  = 'z'
+
+print(letter*10)
+
+x = "helloworld"
+
+print(x.upper())
+
+x = "helloworld"
+
+print(x.lower())
+
+x = "helloworld"
+
+print(x.split())
