78ee8d7e1272d71f9c360ab24cadf4e47500398b
pogikeith <pogikeith@yahoo.com>
2020-01-30 11:36:49 -0700

    fstrings

---
+++ files/formatstring.py
@@ -0,0 +1,41 @@
+print('this is a string {}'.format('INSERT'))
+
+print('The {} {} {}'.format('fox' , 'brown' , 'quick'))
+
+print('The {2} {1} {0}'.format('fox' , 'brown' , 'quick'))
+
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+print('The {f} {f} {f}'.format(f='fox', b='brown', q='quick'))
+
+result = 100/777
+print(result)
+print('the result was {}'.format(result))
+
+result = 100/777
+print(result)
+print('the result was {r}'.format(r=result))
+
+result = 100/777
+print(result)
+print('the result was {r:1.3f}'.format(r=result))
+
+result = 100/777
+print(result)
+print('the result was {r:10.3f}'.format(r=result))
+
+
+name = 'jose'
+print('Hello his name is {}'.format(name))
+
+name = 'jose'
+print(f'Hello his name is {name}')
+
+name = 'jose'
+print(f'Hello his name is {name}')
+
+name = 'jose'
+age = 3
+print(f'{name} is {age} years old')
+
+
