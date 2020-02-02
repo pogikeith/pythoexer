ce4fc69f2fa62a2a2e2c0df4a2f16a813f96a71f
pogikeith <pogikeith@yahoo.com>
2020-02-02 09:20:15 -0700

    tuples

---
+++ files/tuples.py
@@ -0,0 +1,15 @@
+t = (1,2,3,1,2,3,3)
+mylist = [1,2,3]
+print(type(t))      
+print(type(mylist))                      
+print(len(t))       
+print(t[1])
+
+mylist[0] = "NEW"
+print(mylist)
+
+
+b = ('a', 'b', 'a' , 'a')
+print(b.count('a'))
+print(b.index('b'))
+
