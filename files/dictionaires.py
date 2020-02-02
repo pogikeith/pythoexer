5decc6dde5f097cb8d32aed97cf5c97b1f5574dc
pogikeith <pogikeith@yahoo.com>
2020-02-01 17:34:11 -0700

    keys

--- files/dictionaires.py
+++ files/dictionaires.py
@@ -1,3 +1,10 @@
 my_dict = {'key1': 'value1', 'key2' : 'value2'}
 print(my_dict)
 print(my_dict['key1'])
+
+d={'key1': 123, 'k2': [0,1,2]}
+print(d['k2'])
+print(d['key1'])
+
+d['key1': 'new value']
+print(d)
