3c3373a13f07b2241e2c3de0c4ea18f83541e7af
pogikeith <pogikeith@yahoo.com>
2020-02-07 15:40:44 -0700

    moreelif

---
+++ files/elifelse.py
@@ -0,0 +1,40 @@
+hungry = True
+
+if hungry:
+    print('Feed me')
+
+
+
+hungry = False
+
+if hungry:
+    print('Feed me')
+else: 
+    print('not')
+
+hungry = True
+
+if hungry == True:
+    print('Feed me')
+else: 
+    print('not')
+
+
+
+loc = "bank"
+
+if loc == 'auto shop': 
+    print('cars cool')
+else:
+    print('i dont know much')
+
+
+
+loc = "bank"
+
+if loc == 'auto shop': 
+    print('cars cool')
+elif loc == 'bank': 
+    print('money is cool')
+else:
+    print('i dont know much')
