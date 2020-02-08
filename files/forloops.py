f5020651ac47af55be8272cf4d9a541c5543f10d
pogikeith <pogikeith@yahoo.com>
2020-02-08 09:06:47 -0700

    forloop

---
+++ files/forloops.py
@@ -0,0 +1,44 @@
+mylist = [1,2,3,4,5,6,7,8]
+
+for num in mylist:
+    print(num)
+
+
+
+mylist = [1,2,3,4,5,6,7,8]
+
+for num in mylist:
+    print('hello')
+
+
+
+mylist = [1,2,3,4,5,6,7,8]
+for num in mylist:
+    if num % 2 == 0: 
+        print(num)
+    else: 
+        print(f'Odd Number: {num}')
+
+
+
+
+mylist = [1,2,3,4,5,6,7,8]
+
+list_sum = 0
+
+for num in mylist:
+    list_sum = list_sum + num
+print(list_sum)
+
+
+
+mystring = 'hello world'
+
+for letter in mystring:
+    print(letter)
+
+
+for letter in 'hello world':
+    print(letter)
+
+
