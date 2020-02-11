91c022f900c2259d7d10c91cf44acf4e2b4abf58
pogikeith <pogikeith@yahoo.com>
2020-02-11 16:30:36 -0700

    operat

---
+++ files/usefulops.py
@@ -0,0 +1,16 @@
+mylist = [1,2,3]
+
+for num in range(10):
+    print(num)
+
+
+for num in range(3,10):
+    print(num)
+
+
+mylist = [1,2,3,4,5,6,7]
+
+for num in range(0,11,10):
+    print(num)
+
+
