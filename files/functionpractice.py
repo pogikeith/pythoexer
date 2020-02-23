44083c5117532a3e388e3aa49d15111675f22d61
pogikeith <pogikeith@yahoo.com>
2020-02-22 18:44:14 -0700

    nums

--- files/functionpractice.py
+++ files/functionpractice.py
@@ -26,9 +26,12 @@ for num in range(1,10):
         print(num)
 
 
-text = 'home am i'
-def master_yoda(text):
-    return ' '.join(text.split()[::-1])
-    print(master_yoda)
+mylist = [1,2,3,2,3,4,3,4] 
+def has_33(nums):
+    for i in range(0,len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            return True 
+        return False
 
+print(has_33([1,2,3,3]))
   
