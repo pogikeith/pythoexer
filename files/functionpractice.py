ea2c87c0711976102bf5b264834893d72bafccc7
pogikeith <pogikeith@yahoo.com>
2020-02-24 21:47:31 -0700

    testq

--- files/functionpractice.py
+++ files/functionpractice.py
@@ -35,4 +35,13 @@ def has_33(nums):
 
 print(has_33([1,2,3,3]))
   
-# */stop here/*
+
+mylist = [1,2,3,2,3,4,3,4] 
+def has_33(nums):
+    for i in range(0,len(nums)-1):
+        if nums[i: i +2] == [3,3]
+            return True 
+        return False
+
+print(has_33([1,2,3,3]))
+  
