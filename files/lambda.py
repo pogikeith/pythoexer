5e621610b0b418adb577eeaf45739e05ad8995b2
pogikeith <pogikeith@yahoo.com>
2020-03-24 18:55:19 -0600

    lambda

--- files/lambda.py
+++ files/lambda.py
@@ -15,7 +15,31 @@ def splicer(mystring):
 
 
 names = ['andy', 'bob', 'cat']
+print(map(splicer, names))
 
-print(list(map(splicer, names)))
+
+def check_even(num):
+    return num%2 == 0
+mynums = [1,2,3,4,6]
+print(list(filter(check_even, mynums)))
+
+
+def check_even(num):
+    return num%2 == 0
+mynums = [1,2,3,4,6]
+for n in filter(check_even, mynums): 
+    print(n)
+
+
+def square(num):
+    result = num **2 
+    return result
+print(square(3))
+
+
+def square(num):
+     
+    return num **2
+print(square(3))
 
 
